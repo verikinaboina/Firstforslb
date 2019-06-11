fdf3a292699527f9125124c91c22cea92a124d7f
verikinaboina <verikinaboina@slb.com>
2019-06-11 18:43:14 -0400

    click event

--- my-special/src/app/app.component.ts
+++ my-special/src/app/app.component.ts
@@ -4,14 +4,33 @@ import { appContent } from './app.content';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   title = 'turbo';
   public appContent: any;
+  isavailable = false;
 
   ngOnInit() {
     this.appContent = appContent.appComponent;
-    }
+  }
 
+  // myClickFunction(event: any) {
+  //   //just added console.log which will display the event details in browser on click of the button.
+  //   alert("Button is clicked");
+  //   console.log(event);
+  // }
+
+   myClickFunction(event) {
+      this.isavailable = false;
+   }
+
+  //array of months.
+  months = ["January", "Feburary", "March", "April",
+    "May", "June", "July", "August", "September",
+    "October", "November", "December"];
+
+  changemonths(event) {
+    alert("Changed month from the Dropdown");
+  }
 }
